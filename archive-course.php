8831677b3b62156f8e2b2581babc01122fa79cbe
konisys5 <117132028+kr1897@users.noreply.github.com>
2022-11-26 21:18:20 +0900

    コースページ 編集
    ・archive-corse.php
    ・loop-course.php
    編集
    
    小西

--- archive-course.php
+++ archive-course.php
@@ -2,7 +2,7 @@
 ?>
 <?php get_header(); ?>
 
-<h2 class="pageTitle">メニュー<span>MENU</span></h2>
+<h2 class="pageTitle">モデルコース<span>course</span></h2>
 
 <!-- パンくずリストを読み込む -->
 <?php echo do_shortcode('[flexy_breadcrumb]'); ?>
@@ -34,44 +34,15 @@ $purposes = get_terms($args);
 <main class="main">
     <section class="sec">
         <div class="container">
-            <div class="sec_header">
-                <h2 class="title title-jp"><?php echo $kind->name; ?></h2>
-                <span class="title title-en"><?php echo strtoupper($kind->slug); ?></span>
-            </div>
+            <!-- <div class="sec_header">
+                <h2 class="title title-jp"><?php //echo $kind->name; ?></h2>
+                <span class="title title-en"><?php //echo strtoupper($kind->slug); ?></span>
+            </div> -->
 
             <div class="row justify-content-center">
-                <!-- 検索用サブクエリを定義 -->
-                <?php
-                // 投稿タイプ
-                // $args = array(
-                //     'post_type' => 'menu',
-                //     'posts_per_page' => -1,
-                // );
-
-                // 料理の種類で絞り込む
-                // $taxquerysp = array(
-                //     'relation' => 'AND',
-                // );
-                // $taxquerysp[] = array(
-                //     'taxonomy' => 'kind',
-                //     'terms' => $kind->slug,
-                //     'field' => 'slug',
-                // );
-                // $args['tax_query'] = $taxquerysp;
-
-                // print_r($args);
-
-                // サブクエリを生成
-                // $the_query = new WP_Query($args);
-                ?>
-
-                <!-- ループの開始 -->
                 <?php if (have_posts()) : ?>
                 <?php while (have_posts()) : ?>
-                <!-- メニューを取得して$postに代入 -->
                 <?php the_post(); ?>
-
-                <!-- 繰り返しメニューのカード型 -->
                 <div class="col-md-3">
                     <?php get_template_part('template-parts/loop', 'course') ?>
                 </div>
