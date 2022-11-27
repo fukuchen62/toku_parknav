d992bda8430e716f3dbe36dae322f9801d86e108
shibu-yayuki <tunogorou723@gmail.com>
2022-11-27 23:40:08 +0900

    公園の一覧、詳細、ループを編集中
    ①archive-park.phpを追加(仮)
    ②header.phpをさわったが元通りに修復済
    ③searchform-park.phpをさわったが元通りに修復済
    ④single-park.php編集中
    ⑤loop-park.phpを追加
    
    渋谷

--- single-park.php
+++ single-park.php
@@ -5,61 +5,118 @@
 <!-- パンくずリストを読み込む -->
 <?php get_template_part('template-parts/breadcrumb'); ?>
 
+<!-- ループの開始 -->
+<?php //if (have_posts()) :
+?>
+<?php //while (have_posts()) :
+?>
+<!-- 記事を取得して$postに代入 -->
+<?php //the_post();
+?>
+
 <main class="main">
     <section class="sec">
         <div class="container">
             <div class="article article-menu">
                 <div class="row">
                     <div class="col-12 col-md-6">
-                    
 
-                    <!-- 公園タイトル、概要テキスト、写真を読み込む -->
-                    <h2 class="article_title"><?php the_title(); ?></h2>
-                    <div class="content">
-                        <?php the_content(); ?>
+
+                        <!-- 公園タイトル、概要テキスト、写真を読み込む -->
+                        <h2 class="article_title"><?php the_title(); ?></h2>
+                        <div class="content">
+                            <?php the_content(); ?>
+                        </div>
                     </div>
-                </div>
-            
-                <div class="col-12 col-md-6">
-                    <div class="article_pic">
-                        <?php
-                                $pic = get_field('park_image_1');
-                                // 大サイズ画像URL
-                                $pic_url = $pic['sizes']['large'];
-                                ?>
-                        <img src="<?php echo $pic_url; ?>" alt="<?php echo $pic['filename']; ?>">
+                    <?php the_field('discription'); ?>
+
+
+                    <div class="col-12 col-md-6">
+                        <div class="article_pic">
+                            <?php
+                            $pic = get_field('park_image_1');
+                            // 大サイズ画像URL
+                            $pic_url = $pic['sizes']['large'];
+                            ?>
+                            <img src="<?php echo $pic_url; ?>" alt="<?php echo $pic['filename']; ?>">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
         <!--遊具の名前、写真、説明、療育コメントを読み込む  -->
         <div class="col-12 col-md-6">
-            <span><?php the_field('playground_name1'); ?></span>
-                        <div class="article_pic">
-                            <?php
-									$pic = get_field('playground_image1');
-									// 大サイズ画像URL
-									$pic_url = $pic['sizes']['large'];
-									?>
-                            <img src="<?php echo $pic_url; ?>" alt="<?php echo $pic['filename']; ?>">
-                        </div>
-                    </div>
-            
-                    <li>
-                        <b>療育コメント</b>
-                        <span><?php the_field('playground_comment1'); ?></span> 
-                    </li>
-                    <li>
-                        <b>遊具の説明</b>
-                        <span><?php the_field('playground_description1'); ?></span>
-                    </li>
+            <h3><?php the_field('playground_name1'); ?></h3>
+            <div class="article_pic">
+                <?php
+                $pic = get_field('playground_image1');
+                // 大サイズ画像URL
+                $pic_url = $pic['sizes']['large'];
+                ?>
+                <img src="<?php echo $pic_url; ?>" alt="<?php echo $pic['filename']; ?>">
+            </div>
+        </div>
+
+        <li>
+            <b>療育コメント</b>
+            <span><?php the_field('playground_comment1'); ?></span>
+        </li>
+        <li>
+            <b>遊具の説明</b>
+            <span><?php the_field('playground_description1'); ?></span>
+        </li>
+
+
+
+        <div class="col-12 col-md-6">
+            <h3><?php the_field('playground_name2'); ?></h3>
+            <div class="article_pic">
+                <?php
+                $pic = get_field('playground_image2');
+                // 大サイズ画像URL
+                //$pic_url = $pic['sizes']['large'];
+                ?>
+                <img src="<?php echo $pic_url; ?>" alt="<?php echo $pic['filename']; ?>">
+            </div>
+        </div>
+
+        <li>
+            <b>療育コメント</b>
+            <span><?php the_field('playground_comment2'); ?></span>
+        </li>
+        <li>
+            <b>遊具の説明</b>
+            <span><?php the_field('playground_description2'); ?></span>
+        </li>
+
+
+
+        <div class="col-12 col-md-6">
+            <h3><?php the_field('playground_name3'); ?></h3>
+            <div class="article_pic">
+                <?php
+                $pic = get_field('playground_image3');
+                // 大サイズ画像URL
+                //$pic_url = $pic['sizes']['large'];
+                ?>
+                <img src="<?php echo $pic_url; ?>" alt="<?php echo $pic['filename']; ?>">
+            </div>
+        </div>
+
+        <li>
+            <b>療育コメント</b>
+            <span><?php the_field('playground_comment3'); ?></span>
+        </li>
+        <li>
+            <b>遊具の説明</b>
+            <span><?php the_field('playground_description3'); ?></span>
+        </li>
 
 
 
 
-<!-- 公園のデータを読み込む -->
+        <!-- 公園のデータを読み込む -->
         <div class="info">
             <div class="container">
                 <ul class="info_list">
@@ -97,21 +154,16 @@
                     </li>
                     <li>
                         <b>設備など</b>
-                        <span><?php the_field('facility'); ?></span>
-                        <?php
-								if (get_field('facility')) {
-									$msg = '必要あり';
-								} else {
-									$msg = '必要なし';
-								}
-								?>
-                        <span><?php echo $msg; ?></span>
+                        <span>
+                            <?php
+                            $facility = get_field('facility');
+                            echo implode('、', $facility);
+                            ?></span>
                     </li>
                     <li>
                         <b>地図</b>
                         <span><?php the_field('iflame'); ?></span>
                     </li>
-                    
                 </ul>
             </div>
         </div>
@@ -120,33 +172,76 @@
 
 
 
-<!-- モデルコースへのリンク -->
+    <!-- モデルコースへのリンク -->
+    <div class="col-12">
+        <div class="sec_btn">
+            <a href="<?php echo get_permalink(115); ?>" class="btn btn-default">モデルコースのカード型<i class="fas fa-angle-right"></i></a>
+        </div>
+    </div>
 
 
+    <!-- 近くの公園（３つ）カード型を呼び出す -->
+    <!-- 以下参考コード、あとで修正 -->
+    <?php
+    // おすすめの記事一覧を取得
+    //$posts =// get_field('recommend');
+    //if ($posts) :
+    //
+    ?>
+    <div class="post-wrapper">
+        <?php //foreach ($posts as $post) :
+        ?>
+        <div class="post-list">
+            <a href="<?php //echo get_permalink($post->ID);
+                        ?>">
+                <img src="<?php //echo wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+                            ?>">
+                <?php //echo get_the_title($post->ID);
+                ?>
+            </a>
+        </div>
+        <?php //endforeach;
+        ?>
+    </div>
+    <? php // endif;
+    ?>
+</main>
 
-
-
-<!-- 近くの公園（３つ）カード型を呼び出す -->
-<!-- 以下参考コード、あとで修正 -->
+<!--参考その２  -->
 
 <?php
-// おすすめの記事一覧を取得
-$posts = get_field('recommend');
-if( $posts ):
+//$args = array(
+//'post_type' => 'park', // 投稿タイプを指定
+//'orderby' => 'rand', // ランダムで表示
+//'posts_per_page' => 3, // 表示する記事数
+//'post__not_in' => array($post->ID) // 現在表示している記事を除外
+//);
+//$post_query = new WP_Query($args);
+//if ($post_query->have_posts()) :
+//while ($post_query->have_posts()) : $post_query->the_post();
+//
 ?>
-<div class="post-wrapper">
-    <?php foreach( $posts as $post ): ?>
-        <div class="post-list">
-        <a href="<?php echo get_permalink( $post->ID ); ?>">
-        <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>">
-        <?php echo get_the_title( $post->ID ); ?>
-        </a>
-    </div>
-    <?php endforeach; ?>
-</div>
-<?php endif; ?>
+
+<!-- ここにhtml -->
+<? php // endwhile;
+//else :
+//
+?>
+<!-- <p>記事は見つかりませんでした</p> -->
+<? //php
+//endif;
+//wp_reset_postdata();
+?>
 
 
+
+
+
+<?php //endwhile;
+?>
+<!-- ループの終了 -->
+<?php //endif;
+?>
 
 
 <?php //フッターテンプレートファイルを読み込む
