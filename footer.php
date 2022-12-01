71de222e221ed9bb57ac9ddc2983176d89d13816
shibu-yayuki <tunogorou723@gmail.com>
2022-12-01 11:34:20 +0900

    ヘッダーフッターのテンプレート作成中
    ヘッダーフッターのテンプレート作成中
    
    渋谷

--- footer.php
+++ footer.php
@@ -1,57 +1,96 @@
-<div class="pagetop js-pagetop"><i class="fas fa-angle-up"></i>PAGE TOP</div>
-
-<footer class="footer">
-    <div class="container">
-        <div class="footer_inner">
-            <nav>
-                <!-- <ul>
-                    <li><a href="<?php echo home_url(); ?>">HOME</a></li>
-                    <li><a href="#">わたしたちについて</a></li>
-                    <li><a href="#">アクセス</a></li>
-                    <li><a href="#">最新情報</a></li>
-                    <li><a href="#">お問い合わせ</a></li>
-                </ul> -->
-                <?php
-                $args = array(
-                    'menu' => 'footer-menu',
-                    'menu_class' => '',
-                    'container' => false,
-                );
-                wp_nav_menu($args);
-                ?>
-            </nav>
-            <div class="footer_copyright">
-                <small>&copy; とくしまGo!Go!パークなび All rights reserved.</small>
-            </div>
+<!-- フッター -->
+<footer class="foot_bgcolor_cream">
+    <div class="foot_content">
+        <div class="foot_menu">
+            <ul class="foot_col">
+                <li class="foot_fontbold">
+                    公園検索
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/area/east/'); ?>">県東部</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/area/west/'); ?>">県西部</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/area/south/'); ?>">県南部</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/park/'); ?>">条件検索</a>
+                </li>
+            </ul>
+            <ul class="foot_col">
+                <li>
+                    <a href="<?php echo home_url('/course/'); ?>">モデルコース一覧</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/nursing/'); ?>">運動療育について</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/gamestart/'); ?>">ゲーム</a>
+                </li>
+            </ul>
+            <ul class="foot_col">
+                <li>
+                    <a href="<?php echo home_url('/newslist/'); ?>">ニュース一覧</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/mypage/'); ?>">お気に入りリスト</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/about/'); ?>">このサイトについて</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/privacy/'); ?>">プライバシーポリシー・免責事項</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/contact/'); ?>">お問い合わせ</a>
+                </li>
+                <li>
+                    <a href="<?php echo home_url('/minori/'); ?>">サイト制作にあたって</a>
+                </li>
+            </ul>
         </div>
+
+        <?php
+        // $args = array(
+        //     'menu' => 'footer-menu',
+        //     'menu_class' => '',
+        //     'container' => false,
+        // );
+        // wp_nav_menu($args);
+        ?>
+
+        <!-- コピーライト -->
+        <div class="foot_copyright">
+            copyright©とくしまGo!Go!パークなび All Rights Reserved.
+        </div>
+    </div>
+
+    <!-- ポポ二郎の豆知識 -->
+    <div class="popo_trivia hoge">
+        <div class="popo_commentset">
+            <img class="popo_commentbox" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_commentbox.png" alt="ポポ吹き出し">
+            <p id="popo_comment" class="popo_comment"></p>
+        </div>
+        <img class="popo" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_img.png" alt="ポポじろう">
+        <input class="popo" type="image" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_img.png" value="豆知識" onclick="mame()" alt="ポポじろう">
+    </div>
+
+    <!-- トップへ戻るボタン -->
+    <!-- <div class="pagetop"> -->
+    <!-- ボタン本体  -->
+    <div class="foot_top foot_rotate hoge">
     </div>
 </footer>
 
-<!--
-<link href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/home.js"></script> -->
+<!-- スクリプト -->
+<script src="./assets/js/slick.min.js"></script>
+<script src="./assets/js/common.js"></script>
+<script src="./assets/js/top.js"></script>
 
 <?php
-// トップページであれば以下のファイルを読み込む
-// if (is_home()) {
-//     // slickのCSSファイルを読み込む
-//     wp_enqueue_style('slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
-
-//     // slickのJSファイルを読み込む
-//     wp_enqueue_script(
-//         'slick-carousel',
-//         'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js'
-//     );
-
-//     // 独自のJSファイルを読み込む
-//     wp_enqueue_script(
-//         'bistro-calme-home',
-//         get_template_directory_uri() . '/assets/js/home.js'
-//     );
-// }
-
-// wp_footer関数を呼び出す
+// wp_foot()を呼び出す
 wp_footer();
 ?>
 </body>
--- header.php
+++ header.php
@@ -3,79 +3,126 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta name="description" content="休日に親子でお出かけ！「とくしまGo!Go!パークなび」では、お子様との休日に最適な徳島県内の公園情報を発信中。公園で過ごすためのモデルコースや療育情報に沿った遊具の情報も掲載しています。">
+
+    <!-- フォント読み込み-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+
+    <!-- 各種css読み込み -->
+    <!-- <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/slick.css">
+    <link rel="stylesheet" href="./assets/css/slick-theme.css">
+    <link rel="stylesheet" href="./assets/css/common.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
+    <link rel="stylesheet" href="./assets/css/top.css"> -->
+
+    <!-- jQuery -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+
+    <!-- fontawesome -->
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous"> -->
+
+    <!-- <title>とくしまGo!Go!パークなび</title> -->
     <?php
     // wp_head()を呼び出す
     wp_head();
     ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body>
+    <?php body_class(); ?>>
     <?php
     // wp_body_openを呼び出す
     wp_body_open();
     ?>
+    <!-- Gナビ -->
+    <header>
+        <nav class="head_bg">
+            <div class="head_content">
 
-    <header class="header">
-        <div class="header_inner">
-            <div class="header_logo">
-                <h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/logo@2x.png" alt="とくしま GO!GO! パークなび"></a></h1>
+                <!-- ロゴ -->
+                <div>
+                    <a href="<?php echo home_url('') ?>">
+                        <img class="head_logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/key-visual/header_logo.png" alt="とくしまGo!Go!パークなび">
+                    </a>
+                </div>
+
+                <!-- メニュー -->
+                <div class="head_menu_item">
+                    <a href="<?php echo home_url('/park/');
+                                ?>">公園検索</a>
+                    <span>　|　 </span>
+                    <a href="<?php echo home_url('/course/');
+                                ?>">モデルコース</a>
+                    <span>　|　</span>
+                    <a href="<?php echo home_url('/nursing/');
+                                ?>">運動療育について</a>
+                    <span>　|　</span>
+                    <a href="<?php echo home_url('/gamestart/');
+                                ?>">ゲーム</a>
+                    <span>　|　</span>
+                    <a href="<?php echo home_url('/mypage/');
+                                ?>">お気に入りリスト</a>
+
+                    <?php
+                    // $args = array(
+                    //     'menu' => 'global-navigation',
+                    //     'menu_class' => '',
+                    //     'container' => false,
+                    // );
+                    // wp_nav_menu($args);
+                    ?>
+                </div>
+
+                <!-- 検索ボックス -->
+                <div class=" head_menu_item">
+                    <form class="head_search" action="">
+                        <input class="head_form" size="300" type="search" name="search" placeholder="キーワードを入力">
+                        <input class="head_submit fas" type="submit" value="&#xf002;">
+                    </form>
+
+                    <!-- 検索フォームを表示させる -->
+                    <? //php get_search_form();
+                    //php get_template_part('searchform','keywords');
+                    ?>
+                </div>
+
             </div>
 
-            <div class="header_desc">
-                <!-- <p>サイトのキャッチフレーズ</p> -->
-                <p><?php bloginfo('description'); ?></p>
+            <!-- ハンバーガーメニュー -->
+            <div class="hbg_menubox">
+                <input id="hbg" type="checkbox" class="input-hidden" value="hbg_open">
+                <label for="hbg" class="hbg_btn hbg_btn">
+                    <span class="hbg_btn-circle"></span>
+                </label>
+                <div class="hbg_menuwrap hbg_menuwrap-right">
+                    <ul class="hbg_menulist hbg_menulist-circle">
+                        <a href="<?php echo home_url('/park/'); ?>">公園検索</a>
+                        <span>　|　 </span>
+                        <a href="<?php echo home_url('/course/'); ?>">モデルコース</a>
+                        <span>　|　</span>
+                        <a href="<?php echo home_url('/nursing/'); ?>">運動療育について</a>
+                        <span>　|　</span>
+                        <a href="<?php echo home_url('/gamestart/'); ?>">ゲーム</a>
+                        <span>　|　</span>
+                        <a href="<?php echo home_url('/mypage/'); ?>">お気に入りリスト</a>
+
+                        <!-- 検索ボックス -->
+                        <li class="hbg_search_box">
+                            <form class="hbg_search" action="index.php" method="get">
+                                <input class="hbg_form" size="300" type="search" name="search" placeholder="キーワードを入力">
+                                <input class="hbg_submit fas" type="submit" value="検索">
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+                <div class="hbg_cover"></div>
             </div>
-
-            <!-- 検索フォームを表示させる -->
-            <?php get_search_form(); ?>
-        </div>
-
-        <div class="header_links">
-            <nav class="gnav">
-                <!-- <ul class=""> -->
-                <!-- <li><a href="<?php //echo home_url('/'); -->
-                ?>">HOME</a></li>
-                <li><a href="<?php //echo home_url('/about/');
-                                ?>">わたしたちについて</a></li>
-                <li><a href="<?php //echo home_url('/access/');
-                                ?>">アクセス</a></li>
-                <li><a href="<?php //echo home_url('/category/news');
-                                ?>">最新情報</a></li>
-                <li><a href="<?php //echo home_url('/menu/');
-                                ?>">メニュー紹介</a></li>
-                <li><a href="<?php //echo home_url('/contact/');
-                                ?>">お問い合わせ</a></li>
-
-                </ul> -->
-                <?php
-                $args = array(
-                    'menu' => 'global-navigation',
-                    'menu_class' => '',
-                    'container' => false,
-                );
-                wp_nav_menu($args);
-                ?>
-            </nav>
-
-            <ul class="header_sns">
-                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            </ul>
-        </div>
-
-        <svg class="header_menu" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 30 30">
-            <defs>
-                <clipPath id="clip-path">
-                    <rect width="30" height="30" fill="none" />
-                </clipPath>
-            </defs>
-            <g clip-path="url(#clip-path)">
-                <rect class="header_border header_border-1" width="30" height="2" transform="translate(0 0)" />
-                <rect class="header_border header_border-2" width="30" height="2" transform="translate(0 10)" />
-                <rect class="header_border header_border-3" width="30" height="2" transform="translate(0 20)" />
-            </g>
-        </svg>
+        </nav>
     </header>
