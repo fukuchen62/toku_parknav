6a2e70e726adff08109bdf940077249ca970dc1e
shibu-yayuki <tunogorou723@gmail.com>
2022-12-02 19:26:58 +0900

    療育ページ編集中
    療育ページ編集中
    
    渋谷

--- page-nursing.php
+++ page-nursing.php
@@ -2,16 +2,210 @@
 ?>
 <?php get_header(); ?>
 
-<h2 class="pageTitle"><?php the_title(); ?></h2>
-
-<div class="content">
-    <!-- 固定ページの内容を読み込んで表示させる -->
-    <?php the_content(); ?>
-
-
-
-
+<!-- キービジュアルのダミーブロック -->
+<div class="key_wrap">
+    <img class="key_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/key-under/key_under_nursing.jpg" alt="キービジュアル">
+    <div class="key_circle"></div>
 </div>
+
+
+<main>
+    <p class="pkz">トップ　＞　運動療育について</p>
+
+    <div class="nur_ttl">
+        <h2>「ギフテッドの公園での遊び方は、<br>
+            <span class="nurttl_c_p">自由に遊んで</span>もらいながら、<br>
+            <span class="nurttl_c_b">楽しく療育</span>のサポートを受けられる。」
+        </h2>
+        <p>とくしまGo！Go!パークなびでは、公園の遊具に注目し、療育の専門として活動する
+            新田昌広先生監修のもと公園で経験できる遊びや遊具について独自調査をしました。
+            公園での遊びの参考にしてみてくださいね。
+        </p>
+    </div>
+
+    <div class="nur_session_list">
+        <!-- 1 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>代表の自己紹介とギフテッドについて教えてください。</h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-morden2.JPG" alt="運動療育個別写真１枚目">
+
+                <div class="column_flex">
+                    <p class="txt">
+                        放課後等デイサービス　ギフテッド・トレラグループ代表の新田です。私は元々、公立の小学校で約３年間、クラス担任や理科専科教員をしていました。その後は支援学校の非常勤講師や放課後等デイサービスで勤務をし、2013年の暮れに板野町にて当時珍しかった<span class="txt_mark">個別で学習支援や問題行動の改善を図る</span>ギフテッドをオープンしました。現在はギフテッド板野、南二軒屋、藍住。板野町内に運動指導を中心としたトレラという4つの事業所を経営しています。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 2 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>読者の理解を深めるために、そもそも療育とは何か教えていただけますでしょうか？</h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-swing3.JPG" alt="運動療育個別写真2枚目">
+
+                <div class="column_flex">
+                    <p class="txt">療育とは<span class="txt_mark">「治療しながら保育をする」</span>こと。という意味で、様々な障害を持つ子供たちに<span class="txt_mark">それぞれの発達に応じたアプローチ</span>のことを指します。代表的なものでは、音楽療法やモンテッソーリ教育、感覚統合療法などがあります。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 3 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>ギフテッドさんの療育の特色について教えてください。</h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-land8.JPG" alt="運動療育個別写真3枚目">
+
+                <div class="column_flex">
+                    <p class="txt">ギフテッド・トレラグループ最大の特徴は、○○療育教室のように療育方針を決めていないところです。子供さんや保護者との面談や体験利用の様子から、<span class="txt_mark">一人一人に対してオーダーメイドで療育プランを組み立て</span>ていきます。そのために私のような元教員(児童指導員)、保育士、作業療法士、理学療法士、心理士などの<span class="txt_mark">専門的な知識や経験、資格を持つ職員が全施設に常勤職員として在籍</span>しています。また我々だけでは、気が付かない視点も当然ありますから定期的に、県外の有名セラピストの先生方にも子供さんの様子を見てもらい指示を仰ぐこともしています。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 4 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>ギフテッドさんでは、療育の目的で公園に行かれていると思いますが、公園を利用するメリットは何でしょうか？</h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-land9.JPG" alt="運動療育個別写真4枚目">
+
+                <div class="column_flex">
+                    <p class="txt">発達障害は関係なく、<span class="txt_mark">全ての子供の発達には運動が大切</span>です。我々だって霊長類、哺乳類の動物ですから。しかし現代では子供たちが自由に遊べる空き地や里山などがありません。そもそも親が育った環境もすでにその状況で、親ですら外遊びの経験が乏しいのです。<span class="txt_mark">特に四つ這い姿勢やロープを引くような腕や肩、背中を連動させる遊び</span>が発達段階においてはとても大切です。これらを施設内で訓練することも可能ですが、<span class="txt_mark">より自然に近い形で楽しみながら体を動かせるように公園を活用</span>しています。</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 5 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>公園での遊びは療育の観点から子供の発達にどのような役に立っていますか？
+                </h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-climbing2.JPG" alt="運動療育個別写真5枚目">
+
+                <div class="column_flex">
+                    <p class="txt">例えばビジョントレーニング（眼球運動）と、手先の訓練は「鬼ごっこ」をすることで育ちます。人間は走っている時全身が上下に揺れます。<span class="txt_mark">揺れているときにものを見ると前庭覚が働くと同時に眼球運動が生じます。</span>たくさん公園で遊ぶ子どもは<span class="txt_mark">運動時に目をたくさん動かすし、公園の遊具での遊びで手をたくさん使います</span>から、自然と身についていきます。ボールを投げたりすることもいいです。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 6 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>ゲームやテレビを見る時間が増えて、子供の外遊び時間は減り、体力の低下につながると言われてますが。
+                </h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-look.JPG" alt="運動療育個別写真6枚目">
+
+                <div class="column_flex">
+                    <p class="txt">TV番組やゲームは子どもの興味を引くよう研究され、作られていますので、どうしても子どもたちがそっちにいくのは、当たり前だろうと思います。
+                        いくらゲームやTVが面白くても、子どもは自然の中につれていくと、夢中になって遊んでくれるはずです。<span class="txt_mark">大切なのは、親御さんが自然の中に子どもをつれていくこと</span>だと思います。<br>
+
+                        徳島県にはたくさん山があり、森の中で拾った枝で遊んだり、虫を捕ったり木登りができます。<span class="txt_mark">親御さんが虫嫌いで虫取りに出かけないと、子どもも虫が苦手になってしまいます。</span>まずは親御さんがやってみると、子どもは親御さんの行動に興味を持つでしょう。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 7 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>公園での遊びの中で、発達支援(療育)を意識した遊び方でおすすめの遊具はありますか？</h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-swing2.JPG" alt="運動療育個別写真7枚目">
+
+                <div class="column_flex">
+                    <p class="txt">大切なことは親が干渉しないことです。<span class="txt_mark">「遊び」は子供の学びの時間なのに親が「あれをこんな風に使いなさい」というのが最もいけません。</span>子供は公園に遊びに来ているのに親だけがが鼻息荒く「よーしここで体を鍛えてやるぞ！」なんて考えたらシラケるだけです。子供が使いたい遊具を使いたいだけ使わせてあげましょう。あえておすすめをあげるなら、危なそうな遊び、幼稚園でしたら止められそうな使い方がとても体にはいいです。（笑）
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 8 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>家庭内での対応のポイントを教えてください。</h3>
+            </div>
+
+            <div class="row_flex">
+                <img class="park_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-swing.JPG" alt="運動療育個別写真8枚目">
+
+                <div class="column_flex">
+                    <p class="txt">発達障害は、<span class="txt_mark">自閉スペクトラム症、注意欠如多動性障害、学習障害</span>の３つを指します。診断基準にDSM-5というものがあり、<span class="txt_mark"></span>その中では３障害ともに共通して「そのこと(障害)が原因で明確に困りが出ているか」<span class="txt_mark"></span>と問われています。つまり、単純に<span class="txt_mark">みんなと同じ行動が出来ない、学年の他の子と同じことが出来ていないから＝発達障害ではない</span>のです。決して親や教員の目線で、単に他の子と違うというだけで発達障害と決めつけて『普通になる』ことを目標にせず、親ではなく<span class="txt_mark">本人が何に困っているか</span>をしっかりと確認しましょう。</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 9 -->
+        <div class="nur_list">
+            <div class="title_flex">
+                <img class="popo_icon" src="<?php echo get_template_directory_uri(); ?>./assets/img/popo_circle_50.png" alt="ぽぽ次郎アイコン">
+                <h3>療育を受けるためには、どのようにしたらいいでしょうか？手順を教えてください。</h3>
+            </div>
+
+            <div class="row_flex">
+                <div class="column_flex">
+                    <p class="txt">まずは<span class="txt_mark">通われている保育園、幼稚園、学校の先生に相談</span>して、そこから<span class="txt_mark">教育センターや総合療育センターにつないでもらう</span>と良いでしょう。<span class="txt_mark">お住まいの自治体にある障がい福祉課へ相談</span>に行かれるのもよいと思います。また<span class="txt_mark">療育施設の種類によっては必要な申請方法や提出書類が違います</span>のでご自身が気になる、通わせたいと思う事業所があれば直接そこへ行き、現在の悩みや不安を相談してみるのもお勧めです。<br>
+                        本ページで紹介している広場は「ギフテッド＆トレラグループ」の利用者様用のスペースです。本ページでは紹介しきれなかった新田昌広先生の放課後デイサービス等発達支援に関する詳細な情報は下記のバナーリンクから確認できます。ギフテッド、トレラ各事業所の見学なども随時受付をしていますので、是非参考にしてみてください。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="nur_profile_wrap">
+        <div class="nur_profile_flex">
+            <img class="nur_profile_img" src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-gifted-profile.jpg" alt="先生のプロフィール写真">
+            <div class="nur_pro_txt">
+                <h4>新田 昌広（にった まさひろ）先生</h4>
+                <p class="txt">株式会社高徳 代表取締役　ギフテッド代表<br>
+                    徳島市出身、芦屋大学教育学部児童教育学科卒<br>
+                    平成26年春、ギフテッドを板野町大寺にオープン。<br>
+                    現在はギフテッド板野に加えて、南二軒屋、藍住、板野町内に運動指導を中心とする4つの事業所を経営
+                </p>
+            </div>
+        </div>
+
+        <!-- ギフテッドのリンク -->
+        <div class="gifted_wrap">
+            <p>↓ギフテッドのHPへ↓</p>
+            <div class="gifted_link">
+                <a href="https://www.kt-gifted.com/" class="gifted_btn">
+                    <img src="<?php echo get_template_directory_uri(); ?>./assets/img/nursing/R-gifted-logo.jpg" alt="ギフテッドのリンク">
+                </a>
+            </div>
+        </div>
+    </div>
+</main>
+
 <?php //フッターテンプレートファイルを読み込む
 ?>
 <?php get_footer(); ?>
