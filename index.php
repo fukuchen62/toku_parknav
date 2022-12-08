0e40b19cfbb5df161f50465987f0d127cb8a2237
shibu-yayuki <tunogorou723@gmail.com>
2022-12-08 15:09:10 +0900

    indexを編集
    indexを編集
    
    渋谷

--- index.php
+++ index.php
@@ -4,73 +4,57 @@
 
 <!-- キービジュアル -->
 <div class="key_wrap">
-    <img class="key_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/key-under/key_under_search.jpg" alt="キービジュアル">
+    <img class="key_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/key-under/key_img.jpg" alt="">
     <div class="key_circle"></div>
 </div>
 
 <main>
-    <!-- パンくずリストを読み込む -->
     <p class="pkz"><?php echo do_shortcode('[flexy_breadcrumb]'); ?></p>
 
-    <h2 class="h2_under">公園検索</h2>
+    <div class="flex_box">
 
-    <!-- 条件ボタン -->
-    <div class="search_btn_wrap">
-        <?php
-        // タクソノミーpurposeの要素を取得する
-        $args = array(
-            'taxonomy' => 'area',
-            'orderby' => 'slug',
-            'order' => 'asc',
-        );
-        $areas = get_terms($args);
-        ?>
-        <!-- タクソノミーのメニューボタンを作成 -->
-        <?php if (!empty($areas)) :
-        ?>
-        <?php foreach ($areas as $key => $area) : ?>
-        <!-- 地域ボタン -->
-        <div class="search_btn search_btn_north">
-            <a href="<?php echo get_term_link($area); ?>"><?php echo $area->name; ?></a>
-        </div>
-        <?php endforeach; ?>
-        <?php endif; ?>
-        <!-- 詳細検索ボタン -->
-        <div class="search_btn search_btn_terms">
-            <a href="<?php echo home_url('/?search_type=park'); ?>">詳細検索</a>
-        </div>
-    </div>
+        <!-- カードとページをまとめるdiv -->
+        <div>
+            <!-- ↓ここに選んだカテゴリ名が入る。 -->
+            <h2 class="h2_under">お知らせ一覧</h2>
+            <!-- card_flexの指定 -->
+            <div class="card_flex">
 
-    <!-- カードとページをまとめるdiv -->
-    <div>
-        <?php
-        // URLのパラメータからタクソノミの値を取得する
-        $area_slug = get_query_var('area');
-        // タクソノミの値にてそのタクソノミを取得する
-        $area = get_term_by('slug', $area_slug, 'area');
-        ?>
+                <!-- ループの開始 -->
+                <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : ?>
+                <!-- 記事を取得して$postに代入 -->
+                <?php the_post(); ?>
 
-        <h2 class="h2_under"><?php echo $area->name; ?></h2>
-        <div class="card_flex">
-            <!-- ループの開始 -->
-            <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : ?>
-            <?php the_post(); ?>
+                <!-- カード型テンプレートパーツの読み込み -->
+                <?php get_template_part('template-parts/loop', 'post') ?>
 
-            <!-- カード型の呼び出し -->
-            <?php get_template_part('template-parts/loop', 'park') ?>
+                <?php endwhile; ?>
+                <!-- ループの終了 -->
+                <?php endif; ?>
 
-            <?php endwhile; ?>
-            <!-- ループの終了 -->
-            <?php endif; ?>
+            </div>
+
+            <!-- ページネーション -->
+            <?php wp_pagenavi(); ?>
+            <!-- <ul class="pagination">
+                <li class="disabled">
+                    <a href="#"><i class="fas fa-angle-left"></i></a>
+                </li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li>
+                    <a href="#"><i class="fas fa-angle-right"></i></a>
+                </li>
+            </ul> -->
+
         </div>
 
-        <!-- ページネーション -->
-        <?php wp_pagenavi(); ?>
-
+        <!-- カテゴリバー -->
+        <?php get_sidebar('category') ?>
     </div>
 </main>
 
-<?php //フッターテンプレートファイルを読み込む
-?>
+<!-- フッター -->
 <?php get_footer(); ?>
