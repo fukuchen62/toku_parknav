ba04090cf7fabcdb0f8d8f9da5ab864fddf9f701
shimada Yuki <117131998+Yuki-Shimada-504@users.noreply.github.com>
2022-12-08 10:31:13 +0900

    front内のcommonなど修正
    front内のcommonなど修正
    
    島田

--- front/assets/js/common.js
+++ front/assets/js/common.js
@@ -84,18 +84,11 @@ function mame() {
 }
 
 
-// slickスライダー
-
-// $(document).on('ready', function() {
-//   $(".regular").slick({
-//     autoplay: true, // 自動再生を設定
-//     autoplaySpeed: 4000, // 自動再生のスピード（ミリ秒単位）
-//     dots: true // ドットインジケーターの表示
-//   });
-// });
+// 公園詳細ページのslickスライダー
 
 $('.slider').slick({
     autoplay:true,
-    autoplaySpeed:5000,
+    autoplaySpeed: 5000,
+    arrows:true,
     dots:true,
 });
