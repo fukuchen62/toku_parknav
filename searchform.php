c2b72d0c9ffae4f52df9211c611ebe4e6bbe352b
shimada Yuki <117131998+Yuki-Shimada-504@users.noreply.github.com>
2022-12-12 20:19:03 +0900

    検索ボックス更新
    検索ボックス更新
    
    島田

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <!-- 検索ボックス -->
 <form class="head_search" action="<?php echo home_url('/'); ?>">
-    <input class="head_form" size="300" type="text" name="s" value="<?php echo the_search_query(); ?>" placeholder="キーワードを入力" id="clearbutton7">
+    <input class="head_form" size="300" type="text" name="s" value="<?php echo the_search_query(); ?>" placeholder="キーワードを入力">
     <input class="head_submit fas" type="submit" value="&#xf002;">
 </form>
