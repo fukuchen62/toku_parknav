fb37f42496f2c74a15da97ce8998c797ccfcbba7
konisys5 <117132028+kr1897@users.noreply.github.com>
2022-12-12 17:41:01 +0900

    searchform.php 整理

--- searchform.php
+++ searchform.php
@@ -1,10 +1,3 @@
-<!-- <div class=" head_menu_item">
-    <form class="header_search" action="<?php echo home_url('/'); ?>" method="get">
-        <input type="text" placeholder="キーワードを入力" name="s" value="<?php echo the_search_query(); ?>">
-        <i class="fas fa-search"></i>
-    </form>
-</div> -->
-
 <div class=" head_menu_item">
     <form class="head_search" method="get" action="<?php echo home_url('/'); ?>">
         <input type="hidden" name="search_type" value="keywords">
