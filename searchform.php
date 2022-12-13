d60b03258b7f08270063c747f592bfb4a052f62c
konisys5 <117132028+kr1897@users.noreply.github.com>
2022-12-13 16:01:16 +0900

    キーワード検索 修正
    ・function.php
    ・search-keywords.php
    ・searchform.php
    
    小西

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <!-- 検索ボックス -->
 <form class="head_search" action="<?php echo home_url('/'); ?>">
+    <input type="hidden" name="search_type" value="keywords">
     <input class="head_form" size="300" type="text" name="s" value="<?php echo the_search_query(); ?>" placeholder="キーワードを入力">
     <input class="head_submit fas" type="submit" value="&#xf002;">
 </form>
